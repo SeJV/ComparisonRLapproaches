714849f257dfc6d2cbe34df99cfe48ceaf312d81
Sebastian Vogt <sebastian1210@web.de>
2020-12-18 02:35:49 +0100

    MC agent fix

--- agent_methods/mc_control_agent.py
+++ agent_methods/mc_control_agent.py
@@ -2,18 +2,17 @@ import numpy as np
 from agent_methods import AbstractAgent
 
 
-# Off-Policy Monte Carlo-Control
 class MCControlAgent(AbstractAgent):
-    def __init__(self, env, epsilon_start=1.0, epsilon_min=0.0, gamma=0.99, name='MCControlAgent'):
+    def __init__(self, env, epsilon_start=1.0, epsilon_min=0.0, gamma=0.99, alpha=0.01, name='MCControlAgent'):
         super().__init__(env, epsilon_start=epsilon_start, epsilon_min=epsilon_min, name=name)
         self.gamma = gamma
+        self.alpha = alpha
 
         # only discrete environments possible
         self.state_space = self.env.observation_space.n
         self.action_space = self.env.action_space.n
 
         self.q_table = np.zeros((self.state_space, self.action_space))
-        self.count_table = np.zeros((self.state_space, self.action_space))
 
         self.states = []
         self.actions = []
@@ -22,7 +21,6 @@ class MCControlAgent(AbstractAgent):
     def reset(self):
         super().reset()
         self.q_table = np.zeros((self.state_space, self.action_space))
-        self.count_table = np.zeros((self.state_space, self.action_space))
 
         self.states = []
         self.actions = []
@@ -45,16 +43,10 @@ class MCControlAgent(AbstractAgent):
         super().episode_done(epsilon_reduction)
 
         g = 0
-        w = 1
-        for t in range(len(self.actions)):
+        for t in reversed(range(len(self.actions))):
             s_t, a_t, r_t = self.states[t], self.actions[t], self.rewards[t]
-
             g = self.gamma * g + r_t
-            self.count_table[s_t, a_t] += w
-            self.q_table[s_t, a_t] += (w / self.count_table[s_t, a_t]) * (g - self.q_table[s_t, a_t])
-            if a_t != np.argmax(self.q_table[s_t]):
-                break
-            w *= 1 / (1 - self.epsilon)  # TODO: 1-self.epsilon is not exactly the probability of policy choosing a_t
+            self.q_table[s_t, a_t] = self.q_table[s_t, a_t] + self.alpha * (g - self.q_table[s_t, a_t])
 
         self.states = []
         self.actions = []
--- examples/comparison_table_based_methods.py
+++ examples/comparison_table_based_methods.py
@@ -3,14 +3,14 @@ from agent_methods import MCControlAgent, SarsaAgent, QLearningAgent, DoubleQLea
 from train import train_agents
 from utils import visualize_training_results_for_agents
 
-env = FrozenLakeEnv(map_name='8x8')
+env = FrozenLakeEnv()
 
-mc_agent = MCControlAgent(env)
-sarsa_agent = SarsaAgent(env, alpha=0.1)
-q_agent = QLearningAgent(env, alpha=0.1)
-double_q_agent = DoubleQLearningAgent(env, alpha=0.15)
+mc_agent = MCControlAgent(env, alpha=0.01)
+sarsa_agent = SarsaAgent(env, alpha=0.01)
+q_agent = QLearningAgent(env, alpha=0.01)
+double_q_agent = DoubleQLearningAgent(env, alpha=0.01)
 
-stats = train_agents(env, [mc_agent, sarsa_agent, q_agent, double_q_agent], training_steps=10000, repetitions=2)
+stats = train_agents(env, [mc_agent, sarsa_agent, q_agent, double_q_agent], training_steps=20000, repetitions=2)
 visualize_training_results_for_agents(stats, save_fig='table_based_models_frozen_lake.png',
-                                      environment_name='FrozenLake8x8')
+                                      environment_name='FrozenLake')
 
