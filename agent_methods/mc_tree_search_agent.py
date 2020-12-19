65de525d7a8ccdd77178211ba4bbd1b9193030c1
Sebastian Vogt <sebastian1210@web.de>
2020-12-19 12:41:39 +0100

    Epsilon reduction reaches minimum at 80% of training steps, init monte carlo tree search

--- agent_methods/mc_tree_search_agent.py
+++ agent_methods/mc_tree_search_agent.py
@@ -5,37 +5,26 @@ from agent_methods import AbstractAgent
 
 class _Node:
     def __init__(self, move=None, parent=None):
-        self.move = move
-        self.parent = parent
-        self.N = 0
-        self.Q = 0
-        self.children = dict()
-
-    def add_children(self, children):
-        for child in children:
-            self.children[child.move] = child
-
-    @property
-    def value(self, explore=0.5):
-        if self.N == 0:
-            return 0  # alternatively return inf to explore
-        else:
-            return self.Q / self.N + explore * sqrt(2 * log(self.parent.N) / self.N)
+        pass
 
 
 class MCTreeSearchAgent(AbstractAgent):
-    def __init__(self, env, gamma=0.99, name='MCControlAgent'):
+    def __init__(self, env, gamma=0.99, name='MCTreeSearchAgent'):
         super().__init__(env, name=name)
         self.gamma = gamma
 
-        self.root_state = env.reset()
-        self.root = _Node()
+        self.root_node = _Node()
 
-    def select_node(self):
-        node = self.root
-        state = self.env.reset()
+    def reset(self):
+        super().reset()
 
-        while len(node.children) != 0:
-            children = node.children.values()
+    def act(self, observation):
+        return super().act(observation)
+
+    def train(self, s_next, reward, done):
+        super().train(s_next, reward, done)
+
+    def episode_done(self, epsilon_reduction=0, alpha_reduction=0):
+        super().episode_done(epsilon_reduction, alpha_reduction)
 
 
