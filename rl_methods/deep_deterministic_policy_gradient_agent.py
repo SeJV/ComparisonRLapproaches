8407ba25b39cf9432f9803dd4e366620d05f15e0
Sebastian Vogt <sebastian1210@web.de>
2021-01-04 11:22:00 +0100

    Refactoring ddpg

--- rl_methods/deep_deterministic_policy_gradient_agent.py
+++ rl_methods/deep_deterministic_policy_gradient_agent.py
@@ -1,5 +1,6 @@
 from typing import Optional, Tuple, List
 import numpy as np
+from numpy.random import default_rng
 from gym import Env
 from gym.spaces import Box, Discrete
 from collections import deque
@@ -54,39 +55,45 @@ class ReplayBuffer:
 
 
 class OUActionNoise:
-    def __init__(self, mean, std_deviation, theta=0.15, dt=1e-2, x_initial=None):
+    def __init__(self, mean=0, std_deviation=0.2, theta=0.15, dt=1e-2, x_initial=None):
+        """
+        Random noise generator for single values
+        # Formula taken from https://www.wikipedia.org/wiki/Ornstein-Uhlenbeck_process.
+        :param mean:
+        :param std_deviation:
+        :param theta:
+        :param dt:
+        :param x_initial:
+        """
         self.theta = theta
         self.mean = mean
         self.std_dev = std_deviation
         self.dt = dt
         self.x_initial = x_initial
+        self.x_prev = x_initial if x_initial is not None else 0
         self.reset()
 
-    def __call__(self):
-        # Formula taken from https://www.wikipedia.org/wiki/Ornstein-Uhlenbeck_process.
+    def __call__(self) -> float:
+        rng = default_rng()
         x = (
             self.x_prev
             + self.theta * (self.mean - self.x_prev) * self.dt
-            + self.std_dev * np.sqrt(self.dt) * np.random.normal(size=self.mean.shape)
+            + self.std_dev * np.sqrt(self.dt) * rng.normal()
         )
         # Store x into x_prev
         # Makes next noise dependent on current one
         self.x_prev = x
         return x
 
-    def reset(self):
-        if self.x_initial is not None:
-            self.x_prev = self.x_initial
-        else:
-            self.x_prev = np.zeros_like(self.mean)
+    def reset(self) -> None:
+        self.x_prev = self.x_initial if self.x_initial is not None else 0
 
 
 class DeepDeterministicPolicyGradientAgent(AbstractAgent):
-    def __init__(self, env: Env, epsilon: float = 1.0, epsilon_min: Optional[float] = None,
-                 alpha: float = 0.01, alpha_min: Optional[float] = None, gamma: float = 0.99,
-                 train_size: int = 512, actor_shape: List[int] = (256, 256),
-                 critic_shape: dict = None,
-                 buffer_size: int = 100000, auto_store_models: bool = False, name: str = 'DDPGAgent'):
+    def __init__(self, env: Env, epsilon: float = 1.0, epsilon_min: Optional[float] = None, alpha: float = 0.01,
+                 alpha_min: Optional[float] = None, gamma: float = 0.99, train_size: int = 512, tau: float = 0.005,
+                 actor_shape: List[int] = (256, 256), critic_shape: dict = None, buffer_size: int = 100000,
+                 auto_store_models: bool = False, name: str = 'DDPGAgent'):
         """
         This implementation follows closely https://arxiv.org/pdf/1509.02971.pdf and
         https://colab.research.google.com/github/keras-team/keras-io/blob/master/examples/rl/ipynb/ddpg_pendulum.ipynb#scrollTo=5XXLGa-86N8a
@@ -94,17 +101,13 @@ class DeepDeterministicPolicyGradientAgent(AbstractAgent):
         Changes to the implementations where made, so that this agent is easy usable and comparable with other agents
         of this repo.
 
-        :param env:
-        :param epsilon:
-        :param epsilon_min:
-        :param alpha:
-        :param alpha_min:
-        :param gamma:
-        :param train_size:
-        :param actor_shape:
-        :param buffer_size:
-        :param auto_store_models:
-        :param name:
+        :param gamma: Discount of future rewards
+        :param train_size: Sample size of training replay
+        :param actor_shape: Shape of sequential dense actor NN
+        :param critic_shape: Shape of dense critic NN. Has state and action path, which then will be concatenated
+            resulting in dictionary like: {'state_path': [16, 32], 'action_path': [32], 'conc_path': [64, 64]}
+        :param buffer_size: Size of replay buffer, where replay information is stored
+        :param auto_store_models: if true, models of actor and critic will be stored after 10 replays
         """
         super().__init__(env, epsilon=epsilon, epsilon_min=epsilon_min, alpha=alpha,
                          alpha_min=alpha_min, name=name)
@@ -120,30 +123,29 @@ class DeepDeterministicPolicyGradientAgent(AbstractAgent):
         self.s: Optional[np.ndarray] = None
 
         # only continuous 1D state_space possible
-        self.state_space = self.env.observation_space.shape[0]
+        self.state_space: int = self.env.observation_space.shape[0]
         # action space is a single continuous value
-        self.action_space = 1
-        self.upper_bound = self.env.action_space.high[0]
-        self.lower_bound = self.env.action_space.low[0]
+        self.action_space: int = 1
+        self.upper_bound: float = self.env.action_space.high[0]
+        self.lower_bound: float = self.env.action_space.low[0]
 
         self.buffer = ReplayBuffer(buffer_size=buffer_size, train_size=self.train_size,
                                    observation_space=self.state_space, action_space=self.action_space)
 
-        self.actor_model = self.build_actor_model()
-        self.critic_model = self.build_critic_model()
-        self.target_actor_model = self.build_actor_model()
-        self.target_critic_model = self.build_critic_model()
-        self.tau = 0.005
+        self.actor_model = self._build_actor_model()
+        self.critic_model = self._build_critic_model()
+        self.target_actor_model = self._build_actor_model()
+        self.target_critic_model = self._build_critic_model()
+        self.tau = tau
 
         # Learning rate for actor-critic models
-        self.critic_optimizer = Nadam(self.alpha * 2)
+        self.critic_optimizer = Nadam(self.alpha * 2)  # critic optimizer has twice the lr of actor optimizer
         self.actor_optimizer = Nadam(self.alpha)
 
         self._compile_models()
 
-        # noise for exploration
-        std_dev = 0.2
-        self.ou_noise = OUActionNoise(mean=np.zeros(1), std_deviation=float(std_dev) * np.ones(1))
+        # noise for exploration for single values
+        self.ou_noise = OUActionNoise()
 
     def reset(self) -> None:
         super().reset()
@@ -151,11 +153,13 @@ class DeepDeterministicPolicyGradientAgent(AbstractAgent):
         self.buffer = ReplayBuffer(train_size=self.train_size, observation_space=self.state_space,
                                    action_space=self.action_space)
 
-        self.actor_model = self.build_actor_model()
-        self.critic_model = self.build_critic_model()
-        self.target_actor_model = self.build_actor_model()
-        self.target_critic_model = self.build_critic_model()
+        self.actor_model = self._build_actor_model()
+        self.critic_model = self._build_critic_model()
+        self.target_actor_model = self._build_actor_model()
+        self.target_critic_model = self._build_critic_model()
         self._compile_models()
+
+        self.ou_noise.reset()
 
     def act(self, observation: np.ndarray) -> np.ndarray:
         self.s = observation
@@ -174,75 +178,10 @@ class DeepDeterministicPolicyGradientAgent(AbstractAgent):
         self.buffer.record((self.s, self.a, reward, s_next, done))
         if self.buffer.record_counter > self.train_size and self.buffer.record_counter % self.train_size == 0:
             self._replay()
-            self.update_target(self.target_actor_model.variables, self.actor_model.variables)
-            self.update_target(self.target_critic_model.variables, self.critic_model.variables)
+            self._update_target(self.target_actor_model.variables, self.actor_model.variables)
+            self._update_target(self.target_critic_model.variables, self.critic_model.variables)
             if self.buffer.record_counter % (self.train_size * 10) == 0 and self.auto_store_models:
                 self.store_models()
-
-    # This update target parameters slowly
-    # Based on rate `tau`, which is much less than one.
-    @tf.function
-    def update_target(self, target_weights, weights):
-        for (a, b) in zip(target_weights, weights):
-            a.assign(b * self.tau + a * (1 - self.tau))
-
-    def _replay(self):
-        states, actions, rewards, next_states, dones = self.buffer.get_training_sample()
-
-        target_next_actions = self.target_actor_model(next_states, training=True)
-        target_next_q_values = np.array(self.target_critic_model([next_states, target_next_actions], training=True))
-        target_next_q_values[dones] = np.zeros(self.action_space)
-        estimated_q_values = rewards + self.gamma * target_next_q_values
-
-        self.critic_model.fit([states, actions], estimated_q_values, verbose=False)
-
-        with tf.GradientTape() as tape:
-            predicted_actions = self.actor_model(states, training=True)
-            critic_value = self.critic_model([states, predicted_actions], training=True)
-            # Used `-value` as we want to maximize the value given
-            # by the critic for our actions
-            actor_loss = -tf.math.reduce_mean(critic_value)
-        actor_grad = tape.gradient(actor_loss, self.actor_model.trainable_variables)
-        self.actor_optimizer.apply_gradients(
-            zip(actor_grad, self.actor_model.trainable_variables)
-        )
-
-    def build_actor_model(self):
-        # Initialize weights between -3e-3 and 3-e3, so first result won't be -1 or 1 and gradient is not zero
-        last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
-
-        inp = m = Input(self.state_space)
-        for layer in self.actor_shape:
-            m = Dense(layer, 'relu')(m)
-        m = Dense(self.action_space, 'tanh', kernel_initializer=last_init)(m)
-
-        out_shift = (self.upper_bound + self.lower_bound) / 2   # center between upper and lower bound
-        out_scale = (self.upper_bound - self.lower_bound) / 2   # distance from center to bounds
-        out = (m + out_shift) * out_scale
-        model = Model(inputs=inp, outputs=out)
-        return model
-
-    def build_critic_model(self):
-        state_out = state_input = Input(self.state_space)
-        for layer in self.critic_shape['state_path']:
-            state_out = Dense(layer, 'relu')(state_out)
-            state_out = Dense(layer, 'relu')(state_out)
-
-        action_out = action_input = Input(self.action_space)
-        for layer in self.critic_shape['action_path']:
-            action_out = Dense(layer, 'relu')(action_out)
-
-        out = Concatenate()([state_out, action_out])
-        for layer in self.critic_shape['conc_path']:
-            out = Dense(layer, 'relu')(out)
-        out = Dense(1)(out)
-
-        model = Model(inputs=[state_input, action_input], outputs=out)
-        return model
-
-    def _compile_models(self) -> None:
-        self.critic_model.compile(self.critic_optimizer, loss='mse')
-        # self.actor_model.compile('Nadam', loss='mse') # TODO! how to implement -value loss?
 
     def store_models(self) -> None:
         self.target_actor_model.save(f'models/{self.name}/actor_model')
@@ -265,4 +204,66 @@ class DeepDeterministicPolicyGradientAgent(AbstractAgent):
 
         self._compile_models()
 
+    # This update target parameters slowly
+    # Based on rate `tau`, which is much less than one.
+    @tf.function
+    def _update_target(self, target_weights, weights):
+        for (a, b) in zip(target_weights, weights):
+            a.assign(b * self.tau + a * (1 - self.tau))
+
+    def _replay(self):
+        states, actions, rewards, next_states, dones = self.buffer.get_training_sample()
+
+        target_next_actions = self.target_actor_model(next_states, training=True)
+        target_next_q_values = np.array(self.target_critic_model([next_states, target_next_actions], training=True))
+        target_next_q_values[dones] = np.zeros(self.action_space)
+        estimated_q_values = rewards + self.gamma * target_next_q_values
+
+        self.critic_model.fit([states, actions], estimated_q_values, verbose=False)
+
+        def actor_loss_function(_, y_predicted):
+            critic_value = self.critic_model([states, y_predicted], training=True)
+            # Used `-value` as we want to maximize the value given
+            # by the critic for our actions
+            return -tf.math.reduce_mean(critic_value)
+
+        self.actor_model.compile(optimizer=self.actor_optimizer, loss=actor_loss_function)
+        self.actor_model.fit(states)
+
+    def _build_actor_model(self) -> Model:
+        # Initialize weights between -3e-3 and 3-e3, so first result won't be -1 or 1 and gradient is not zero
+        last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
+
+        inp = m = Input(self.state_space)
+        for layer in self.actor_shape:
+            m = Dense(layer, 'relu')(m)
+        m = Dense(self.action_space, 'tanh', kernel_initializer=last_init)(m)
+
+        out_shift = (self.upper_bound + self.lower_bound) / 2   # center between upper and lower bound
+        out_scale = (self.upper_bound - self.lower_bound) / 2   # distance from center to bounds
+        out = (m + out_shift) * out_scale
+        model = Model(inputs=inp, outputs=out)
+        return model
+
+    def _build_critic_model(self) -> Model:
+        state_out = state_input = Input(self.state_space)
+        for layer in self.critic_shape['state_path']:
+            state_out = Dense(layer, 'relu')(state_out)
+            state_out = Dense(layer, 'relu')(state_out)
+
+        action_out = action_input = Input(self.action_space)
+        for layer in self.critic_shape['action_path']:
+            action_out = Dense(layer, 'relu')(action_out)
+
+        out = Concatenate()([state_out, action_out])
+        for layer in self.critic_shape['conc_path']:
+            out = Dense(layer, 'relu')(out)
+        out = Dense(1)(out)
+
+        model = Model(inputs=[state_input, action_input], outputs=out)
+        return model
+
+    def _compile_models(self) -> None:
+        self.critic_model.compile(self.critic_optimizer, loss='mse')
+        # self.actor_model.compile('Nadam', loss='mse') # TODO! how to implement -value loss?
 
